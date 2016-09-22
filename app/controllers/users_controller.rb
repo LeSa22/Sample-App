4cc06876ca6d2b4db81e11d72854b86a0a3e5653
LeSa22 <tranlesa22@gmail.com>
2016-09-22 22:51:52 +0700

    Account activation and password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
      redirect_to(root_url) unless current_user.admin?
    end
   def index
-    @users = User.paginate(page: params[:page]) 
+    @users = User.paginate(page: params[:page],  :per_page => 10) 
   end
   def destroy
     User.find(params[:id]).destroy
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
   def show
   	@user = User.find(params[:id]) 
-  #	debugger
+  	#debugger
   end
   def new
   	@user = User.new
@@ -23,9 +23,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		log_in @user
-  		flash[:success] = "Welcome to the Sample App !"
-  		redirect_to @user
+  		@user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
   	else 
   		render 'new'
   	end
@@ -42,6 +42,9 @@ class UsersController < ApplicationController
   		render 'edit'
   	end
   end
+
+
+
   private 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
